d5c8895e3152d4dc0539ba3c6f077da3396b7873
Sultan9463 <s75696643@gmail.com>
2025-05-17 17:27:27 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # Demander les deux nombres à l'utilisateur
-read -p "Entrez le premier nombre : " nombre1
-read -p "Entrez le deuxième nombre : " nombre2
+read -p "Entrez le premier nombre : " num1
+read -p "Entrez le deuxième nombre : " num2
 
-# Demander l'opération à effectuer
+# Affichage du menu
 echo "Choisissez une opération :"
-echo "1. Addition (+)"
-echo "2. Soustraction (-)"
-echo "3. Multiplication (*)"
-echo "4. Division (/)"
-read -p "Votre choix (1/2/3/4) : " choix
+echo "1) Addition"
+echo "2) Soustraction"
+echo "3) Multiplication"
+echo "4) Division"
 
-# Effectuer l'opération choisie
+# Lire le choix de l'utilisateur
+read -p "Votre choix : " choix
+
 case $choix in
     1)
-        resultat=$((nombre1 + nombre2))
-        echo "$nombre1 + $nombre2 = $resultat"
+        echo "Résultat : $((num1 + num2))"
         ;;
     2)
-        resultat=$((nombre1 - nombre2))
-        echo "$nombre1 - $nombre2 = $resultat"
+        echo "Résultat : $((num1 - num2))"
         ;;
     3)
-        resultat=$((nombre1 * nombre2))
-        echo "$nombre1 * $nombre2 = $resultat"
+        echo "Résultat : $((num1 * num2))"
         ;;
     4)
-        resultat=$((nombre1 / nombre2))
-        echo "$nombre1 / $nombre2 = $resultat"
+        if [ "$num2" -ne 0 ]; then
+            echo "Résultat : $((num1 / num2))"
+        else
+            echo "Erreur : Division par zéro"
+        fi
         ;;
     *)
-        echo "Erreur : Choix invalide."
-        exit 1
+        echo "Choix invalide"
         ;;
 esac
