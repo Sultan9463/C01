3661fddb8146e0b63667ae1e64e89253300be125
Sultan9463 <s75696643@gmail.com>
2025-05-17 17:50:48 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
+read -p "Entrez le premier nombre A: " A
+read -p "Entrez le deuxième nombre B: " B
+read -p "Entrez l'opération (+, -, *, /) : " op
 
-# Demander les deux nombres à l'utilisateur
-read -p "Entrez le premier nombre : " num1
-read -p "Entrez le deuxième nombre : " num2
-
-# Affichage du menu
-echo "Choisissez une opération :"
-echo "1) Addition"
-echo "2) Soustraction"
-echo "3) Multiplication"
-echo "4) Division"
-
-# Lire le choix de l'utilisateur
-read -p "Votre choix : " choix
-
-case $choix in
-    1)
-        echo "Résultat : $((num1 + num2))"
-        ;;
-    2)
-        echo "Résultat : $((num1 - num2))"
-        ;;
-    3)
-        echo "Résultat : $((num1 * num2))"
-        ;;
-    4)
-        if [ "$num2" -ne 0 ]; then
-            echo "Résultat : $((num1 / num2))"
-        else
-            echo "Erreur : Division par zéro"
-        fi
-        ;;
-    *)
-        echo "Choix invalide"
-        ;;
-esac
+if [ "$op" == "+" ]; then
+    echo "Addition = $(( A + B ))"
+elif [ "$op" == "-" ]; then
+    echo "Soustraction = $(( A - B ))"
+elif [ "$op" == "*" ]; then
+    echo "Multiplication = $(( A * B ))"
+elif [ "$op" == "/" ]; then
+    echo "Division = $(( A / B ))"
+fi
