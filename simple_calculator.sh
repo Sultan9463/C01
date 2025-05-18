2ccefddf04607f682de5e4174a58297508657db8
Sultan9463 <s75696643@gmail.com>
2025-05-18 14:58:46 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -2,13 +2,10 @@
 # Demander à l'utilisateur de saisir les deux nombres et l'opérateur
 echo "Entrez le premier nombre :"
 read num1
-
 echo "Entrez l'opérateur (+, -, *, /) :"
 read op
-
 echo "Entrez le second nombre :"
 read num2
-
 # Effectuer l'opération en fonction de l'opérateur
 case $op in
     +)
@@ -21,10 +18,6 @@ case $op in
         result=$(echo "$num1 * $num2" | bc)
         ;;
     /)
-        if [ "$num2" -eq 0 ]; then
-            echo "Erreur : Division par zéro impossible"
-            exit 1
-        fi
         result=$(echo "$num1 / $num2" | bc)
         ;;
     *)
