72e7f02029b2fc20a3d6d081de94c15bb11d2b50
Sultan9463 <s75696643@gmail.com>
2025-05-18 19:46:12 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
-# Demander à l'utilisateur de saisir les deux nombres et l'opérateur
-read num1
-read op
-read num2
-# Effectuer l'opération en fonction de l'opérateur
-case $op in
+
+# Vérification du nombre d'arguments
+if [ $# -ne 3 ]; then
+    echo "Utilisation : $0 nombre1 nombre2 opérateur"
+    echo "Exemple : $0 5 3 +"
+    exit 1
+fi
+
+# Récupération des arguments
+nombre1=$1
+nombre2=$2
+operateur=$3
+
+# Vérification que les nombres sont bien des entiers
+if ! [[ "$nombre1" =~ ^-?[0-9]+$ ]] || ! [[ "$nombre2" =~ ^-?[0-9]+$ ]]; then
+    echo "Erreur : les deux premiers arguments doivent être des nombres entiers."
+    exit 1
+fi
+
+# Calcul en fonction de l'opérateur
+case "$operateur" in
     +)
-        result=$(echo "$num1 + $num2" | bc)
+        resultat=$((nombre1 + nombre2))
         ;;
     -)
-        result=$(echo "$num1 - $num2" | bc)
+        resultat=$((nombre1 - nombre2))
         ;;
-    *)
-        result=$(echo "$num1 * $num2" | bc)
+    \*)
+        resultat=$((nombre1 * nombre2))
         ;;
     /)
-        result=$(echo "$num1 / $num2" | bc)
+        if [ "$nombre2" -eq 0 ]; then
+            echo "Erreur : division par zéro."
+            exit 1
+        fi
+        resultat=$((nombre1 / nombre2))
         ;;
     *)
-        echo "Opérateur invalide"
+        echo "Erreur : opérateur non supporté. Utilisez +, -, * ou /."
+        exit 1
         ;;
 esac
 
-# Afficher le résultat
-echo "Résultat est :$result"
+# Affichage du résultat
+echo "Résultat : $resultat"
