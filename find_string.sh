ca52c5c1bf55a41aefcc9408b4b8d6809b9ad143
Sultan9463 <s75696643@gmail.com>
2025-05-19 02:49:54 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-read -p "Entrez le nom du fichier:" fichier
-read -p "Entrez la chaîne à recherche:" chaine
-#recherche de la chaine dans le fichier
-if grep -n "$chaine" "$fichier"; then 
-echo "la chaîne'$chaine'a été trouvée dans $fichier."
+read -p "Entrez le nom du fichier : " fichier
+read -p "Entrez la chaîne à rechercher : " chaine
+# Vérifie si le fichier existe avant de rechercher
+if [[ -f "$fichier" ]]; then
+    # Recherche de la chaîne avec affichage des numéros de ligne
+    if grep -n "$chaine" "$fichier"; then
+        echo "La chaîne '$chaine' a été trouvée dans $fichier."
+    else
+        echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
+    fi
 else
-echo "la chaîne'$chaine'n'a pas été trouvée dans $fichier."
+    echo "Le fichier '$fichier' n'existe pas."
 fi
