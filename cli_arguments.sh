621b45f7a34d6c65b1119aae82db71612bcfe7e8
Sultan9463 <s75696643@gmail.com>
2025-05-19 02:29:17 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 # Afficher chaque argument avec son numéro
 # Vérifie si des arguments ont été passés
-if [ "$#" -eq 0 ]; then
-    echo "Aucun argument fourni. Entrez les arguments séparés par des espaces :"
-    read -a args
-else
-    args=("$@")
-fi
+    read -a "args"
 # Affichage des arguments
 i=1
 for arg in "${args[@]}"; do
-    echo "Argument $i:$arg"
+    echo "Argument $i: $arg"
     ((i++))
 done
