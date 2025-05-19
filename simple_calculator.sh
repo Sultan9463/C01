1ddabf229b8c14b8fe752e9119d43b6de6b98746
Sultan9463 <s75696643@gmail.com>
2025-05-19 03:13:56 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,39 +1,13 @@
 #!/bin/bash
+# Lire l'expression de l'utilisateur
+read -p "Entrez l'expression (ex: 4 2 +):" expression
+# Évaluer l'expression
+result=$(echo "$expression" | awk '{ 
+    if ($3 == "+") print $1 + $2;
+    else if ($3 == "-") print $1 - $2;
+    else if ($3 == "*") print $1 * $2;
+    else if ($3 == "/") print $1 / $2;
+    else print "Opérateur non supporté";
+}')
 
-# Vérification du nombre d'arguments
-if [ $# -ne 3 ]; then
-    echo "Utilisation : $0 nombre1 nombre2 opérateur"
-    echo "Exemple : $0 5 3 +"
-    exit 1
-fi
-
-# Récupération des arguments
-nombre1=$1
-nombre2=$2
-operateur=$3
-# Calcul en fonction de l'opérateur
-case "$operateur" in
-    +)
-        resultat=$((nombre1 + nombre2))
-        ;;
-    -)
-        resultat=$((nombre1 - nombre2))
-        ;;
-    \*)
-        resultat=$((nombre1 * nombre2))
-        ;;
-    /)
-        if [ "$nombre2" -eq 0 ]; then
-            echo "Erreur : division par zéro."
-            exit 1
-        fi
-        resultat=$((nombre1 / nombre2))
-        ;;
-    *)
-        echo "Erreur : opérateur non supporté. Utilisez +, -, * ou /."
-        exit 1
-        ;;
-esac
-
-# Affichage du résultat
-echo "Résultat: $resultat"
+echo "Résultat : $result"
