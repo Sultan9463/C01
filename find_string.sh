ebe5e1d1ee58b44e5fab3b57f513016db0a3c0f8
Sultan9463 <s75696643@gmail.com>
2025-05-19 02:58:45 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -p "Entrez le nom du fichier :"fichier
-read -p "Entrez la chaîne à rechercher :"chaine
+read -p "Entrez le nom du fichier:" fichier
+read -p "Entrez la chaîne à rechercher:" chaine
     # Recherche de la chaîne avec affichage des numéros de ligne
     if grep -n "$chaine" "$fichier"; then
         echo "La chaîne '$chaine' a été trouvée dans $fichier."
